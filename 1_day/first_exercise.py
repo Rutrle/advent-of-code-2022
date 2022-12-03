e359abc3d8e45dca0fe25a68d6221ac472f1cfb3
Jakub Rutrle <jakubrutrle@gmail.com>
2022-12-03 12:55:29 +0100

    first exercise of first day

--- 1_day/first_exercise.py
+++ 1_day/first_exercise.py
@@ -1,6 +1,17 @@
 def count_calories():
-    with open('input.txt','r') as inputfile:
-        pass
+    with open('1_day\input.txt','r') as inputfile:
+        lines = inputfile.readlines()
+        elf_calories_max = 0
+        current_elf_calories = 0
+        for line in lines:
+            if line == "\n":
+                if current_elf_calories > elf_calories_max:
+                    elf_calories_max = current_elf_calories
+                current_elf_calories = 0
+            else:
+                current_elf_calories += int(line)
+        
+        print(elf_calories_max)
 
 
 

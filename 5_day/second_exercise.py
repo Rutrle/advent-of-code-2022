6848b1278ad4a4249c00d9cfd7b193138bfdf47a
Jakub Rutrle <jakubrutrle@gmail.com>
2022-12-08 00:30:17 +0100

    ugly solution of second problem from day 5

---
+++ 5_day/second_exercise.py
@@ -0,0 +1,36 @@
+import re
+from copy import copy
+
+def duplicate_sectors():
+    with open(r'5_day\input.txt',mode='r') as input_file:
+        lines = input_file.readlines()
+
+        piles_num = 9
+        big_ugly_list=[[] for i in range(piles_num)]
+
+        for line in lines[:8]:
+            current_chars = list(line[1::4])
+            print(current_chars)
+            for i, char in enumerate(current_chars):
+                if char != ' ':
+                    big_ugly_list[i].insert(0,char)
+
+        for line in lines[10:]:
+            move =int(re.search("move ([0-9]+)", line).group(1))
+            from_column =int(re.search("from ([0-9]+)", line).group(1))
+            to_column = int(re.search("to ([0-9]+)", line).group(1))
+
+            intermediary_list = []
+            big_ugly_list[to_column-1] += big_ugly_list[from_column-1][-move:]
+            for _ in range(move):
+                intermediary_list.append(big_ugly_list[from_column-1].pop())
+                
+
+
+
+        print(big_ugly_list)
+        for smaler_list in big_ugly_list:
+            print(smaler_list[-1])
+
+if __name__ == "__main__":
+    duplicate_sectors()
